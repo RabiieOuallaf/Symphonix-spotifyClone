ab2d8b23033f6372670cd4a6ed5b9603dd06e69d
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-02-21 14:59:41 +0100

    user authentication and routing

---
+++ app/Http/Controllers/Users.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class Users extends Controller
+{
+    // Load the register page 
+    public function registerPage() {
+        return view('users.register');
+    }
+    // load the login page 
+
+    public function loginPage() {
+        return view('users.login');
+    }
+    
+
+
+    // store the user's data (register)
+    public function storeUserData(Request $request) {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:2'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => 'required'
+        ]);
+
+        // Hash the password 
+
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        // Create the user 
+
+        $user = User::create($formFields);
+
+        // Login 
+
+        auth()->login($user);
+
+        // Redirect the user to the main page 
+
+        return redirect('/')->with('message', 'You logged in seccussfuly!');
+    }
+
+    // validate user's credentials (login)
+
+    public function credentialsVerification(Request $request) {
+
+        $formFields = $request->validate([
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($formFields)) {
+            $request->session()->regenerate();
+
+            return redirect('/')->with('message', 'Welcome back!');
+        }
+
+        return back()->withErrors(['email' => 'invalid credentials']);
+    }
+
+    // logout 
+
+    public function logOut(Request $request) {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return back()->with('message', 'You logedout!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\User;
+use app\Http\Controllers\Bands;
+use app\Http\Controllers\Musics;
+use app\Http\Controllers\Clients;
+use app\Http\Controllers\Artists;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +22,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Register routing 
+Route::get('/register' , [Users::class , 'RegisterPage']);
+Route::post('/register/authentication', [User::class], 'storeUserData');
+
+// Login routing 
+
+Route::get('/login', [Users::class, 'loginPage']);
+Route::post('/login/authentication', [Users::class, 'credentialsVerification']);
+
+// logout route
+Route::get('/logout', [Users::class, 'logout']);
