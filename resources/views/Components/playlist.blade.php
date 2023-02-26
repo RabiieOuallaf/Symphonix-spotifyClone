0f0e06d24d7e333fe9c23b33a7f3dc9f02c1d538
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-02-26 14:06:18 +0100

    playlist component

--- resources/views/Components/playlist.blade.php
+++ resources/views/Components/playlist.blade.php
@@ -1,19 +1,25 @@
-<section class="playlists w-[50%] grid grid-cols-4 grid-gap-10">
+<section class="playlists w-[50%] ">
 
-    <div class="playlist">
-        playlist
+    <h2 class="text-3xl font-semibold text-white my-5" style="margin-left: -5rem;">Made for you </h2>
+    <div class="playlists-container grid grid-cols-4 grid-gap-10">
+
+        <div class="playlist">
+            playlist
+        </div>
+    
+        <div class="playliss">
+            playlist
+        </div>
+    
+        <div class="playlist">
+            playlist
+        </div>
+    
+        <div class="playlist">
+            playlist
+        </div>
+        
     </div>
 
-    <div class="playliss">
-        playlist
-    </div>
-
-    <div class="playlist">
-        playlist
-    </div>
-
-    <div class="playlist">
-        playlist
-    </div>
 
 </section>
